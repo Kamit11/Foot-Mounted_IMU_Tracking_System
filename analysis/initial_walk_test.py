b5fa612861b895a7a4bd6193811421c762efe227
AMIT_LAPTOP\kerub <kerubamit@gmail.com>
2026-08-08 22:24:17 +0300

    Added walk test and heel strike test data.

--- analysis/initial_walk_test.py
+++ analysis/initial_walk_test.py
@@ -2,17 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import arduino_logger as al
 
-N = 12000
-prefix = "initial_walk_test"
+N = 3000
+prefix = "heel_strike_test"
 header = "t_us,seq,ax,ay,az,gx,gy,az,valid,t_imu_us,t_serial_us,missed"
 
-csv_filepath = al.collect_serial_data(header = header, file_prefix=prefix, num_samples=N)
+# csv_filepath = al.collect_serial_data(header = header, file_prefix=prefix, num_samples=N, port='COM3')
+csv_filepath = "data/temp_data/heel_strike_test_08-08-2026_22-19-25.csv"
 
 
 d = np.loadtxt(csv_filepath, delimiter=',', skiprows=1)
 t_us,seq,ax,ay,az,gx,gy,gz,valid,t_imu_us,t_serial_us,missed = d.T
 
 mag = np.sqrt(ax**2 + ay**2 + az**2)
+avg_mag = mag.mean()
 fig, axs = plt.subplots(2, 1, figsize=(12, 8), sharex=True)
 
 # Top Plot: Individual Axes
@@ -22,7 +24,7 @@ axs[0].plot(t_us, az, label='Accel Z', alpha=0.7)
 axs[0].axhline(16, color='r', linestyle='--', label='+16g Limit')
 axs[0].axhline(-16, color='r', linestyle='--', label='-16g Limit')
 axs[0].set_ylabel("Acceleration (g)")
-axs[0].set_title("Walk Test: Raw Accelerometer Axes")
+axs[0].set_title("Heel Strike Test: Raw Accelerometer Axes")
 axs[0].legend()
 
 # Bottom Plot: Total Magnitude
@@ -30,7 +32,9 @@ axs[1].plot(t_us, mag, label='Magnitude', color='purple')
 axs[1].axhline(16, color='r', linestyle='--', label='+16g Limit')
 axs[1].set_xlabel("Time (seconds)")
 axs[1].set_ylabel("Acceleration (g)")
-axs[1].set_title("Walk Test: Total Magnitude")
+axs[1].set_title("Heel Strike Test: Total Magnitude")
+axs[1].axhline(avg_mag, color='y', linestyle='--', label=f"Mean: {avg_mag:.4f} g")
+axs[1].axhline(mag.max(), color='g', linestyle='--', label=f"Max: {mag.max():.4f} g")
 axs[1].legend()
 
 plt.tight_layout()
